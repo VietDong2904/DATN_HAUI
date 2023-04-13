22db900ba1e319cba5a94c42ad920597b4d6dea3
VietDong2904 <dovietdong2904@gmail.com>
2023-04-13 23:18:24 +0700

    Initialize Model and Helpers back

---
+++ BACKEND/Common/Common/Helpers/CertificateUltils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Helpers
+{
+    public static class CertificateUltils
+    {
+        public static RSA CreateRsaPublicKey(X509Certificate2 certificate)
+        {
+            RSA publicKeyProvider = certificate.GetRSAPublicKey();
+            return publicKeyProvider;
+        }
+
+        public static RSA CreateRsaPrivateKey(X509Certificate2 certificate)
+        {
+            RSA privateKeyProvider = certificate.GetRSAPrivateKey();
+            return privateKeyProvider;
+        }
+    }
+}
